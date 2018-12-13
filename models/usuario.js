4c9d7b195d0fd9bac4b16d80af77fa37fab04244
Mike <mgl.angel89@gmail.com>
2018-12-13 01:08:12 -0600

    Usuario y token listo

---
+++ models/usuario.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var Schema = mongoose.Schema;
+
+var rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} rol invalido'
+};
+
+var usuarioSchema = new Schema({
+
+    nombre: { type: String, required: [true, 'Nombre requerido'] },
+    email: { type: String, unique: true, required: [true, 'El email es requerido'] },
+    password: { type: String, required: [true, 'La contraseña es requerida'] },
+    img: { type: String, required: false },
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
+
+});
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
